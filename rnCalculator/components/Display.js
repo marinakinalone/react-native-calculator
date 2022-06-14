0f6417a850167c2080ee07cc843272ba08f977ab
Marina Kinalone Simonnet <kinalone.dev@gmail.com>
2022-06-14 11:14:32 +0200

    fix display overflow

--- rnCalculator/components/Display.js
+++ rnCalculator/components/Display.js
@@ -5,7 +5,9 @@ import { View, Text, StyleSheet } from 'react-native';
 const Display = ({result, theme}) => {
   return (
     <View style={theme === 'light' ? (styles.display__light):(styles.display__dark)}>
-          <Text style={theme === 'light' ? (styles.display__result_light):(styles.display__result_dark)}>{result}</Text>
+          <View style={theme === 'light' ? (styles.display__result_light):(styles.display__result_dark)}>
+            <Text style={theme === 'light' ? (styles.result__light):(styles.result__dark)}>{result}</Text>
+          </View>
     </View>
   )
 }
@@ -17,7 +19,7 @@ const styles = StyleSheet.create({
         marginRight: 5,
         marginBottom: 10,
         borderRadius: 20,
-        boxShadow: "inset 7px 7px 13px #b2c6cf, inset -7px -7px 13px #ecffff"
+        boxShadow: "inset 7px 7px 13px #b2c6cf, inset -7px -7px 13px #ecffff",
     },
     display__dark: {
       marginTop: 70,
@@ -29,21 +31,32 @@ const styles = StyleSheet.create({
     },
     display__result_light: {
         margin: 20,
-        fontSize: 50,
-        color: "#936bb4",
-        textAlign:"right",
+        display: "flex",
+        alignItems: "flex-end",
         paddingRight:20,
         borderRadius: 20,
-        boxShadow: "7px 7px 13px #b2c6cf, 7px -7px 13px #ecffff"
+        boxShadow: "7px 7px 13px #b2c6cf, 7px -7px 13px #ecffff",
+        overflow: "hidden",
+        whiteSpace: "nowrap"
     },
     display__result_dark: {
       margin: 20,
-      fontSize: 50,
-      color: "#cfe6f1",
+      display: "flex",
+      alignItems: "flex-end",
       textAlign:"right",
       paddingRight:20,
       borderRadius: 20,
-      boxShadow: "7px 7px 13px #7d5b99, 7px -7px 13px #a97bcf"
-  }
+      boxShadow: "7px 7px 13px #7d5b99, 7px -7px 13px #a97bcf",
+      overflow: "hidden",
+      whiteSpace: "nowrap"
+  },
+  result__light: {
+    fontSize: 50,
+    color: "#936bb4",
+  },
+  result__dark: {
+    fontSize: 50,
+    color: "#cfe6f1",
+  },
   });
 export default Display;
